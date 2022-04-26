43cf04cd1bdc9b74e9da42f239cd2d0560f2adb0
hasanTheBest <rainbowmahmud@gmail.com>
2022-04-27 03:11:44 +0600

    hour hand position fixed

--- js/script.js
+++ js/script.js
@@ -17,8 +17,8 @@ function updateClockHand() {
   styles(select(".minute-hand"), minutes * 6 + 270 + 1 + (seconds + 1) / 10);
 
   // update hours
-  const degree = hours > 12 ? 30 : 15;
-  styles(select(".hour-hand"), hours * degree + 270 + 1 + (minutes + 1) / 2);
+  // const degree = hours > 12 ? 30 : 15;
+  styles(select(".hour-hand"), hours * 30 + 270 + 1 + (minutes + 1) / 10);
 
   // const degree = hours > 12 ? 15 : 30;
   // styles(select(".hour-hand"), hours * 30 + 271);
